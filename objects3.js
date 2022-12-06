5f24f5e5183c04bc9f47bcfc05a935419f55035f
dopezinho <opugnero@gmail.com>
2022-12-06 18:59:59 -0300

    irirei

---
+++ objects3.js
@@ -0,0 +1,18 @@
+var casinha = [
+    {
+        face: 'sanada',
+        pelo: 'do olho',
+    },
+    {
+        face: 'sarada',
+        pelo: 'do dedo',
+    },
+    {
+        face: 'susuna',
+        pelo: 'da unha do pé',
+    }
+]
+
+console.log(casinha[1].face)
+console.log(casinha[2].face)
+console.log(casinha[0].face)
