f853766605e79b25d82e850127f748a8e7b04156
dopezinho <opugnero@gmail.com>
2022-12-06 18:48:07 -0300

    rachou

---
+++ objects.js
@@ -0,0 +1,16 @@
+let playa = {
+    nominho: 'Leenguiceto',
+    idage: 498,
+    racho: 39,
+    Gadou(c=0) {
+        console.log('Leenguiceto rachooo jiojiojiq')
+        this.racho += c
+    }
+}
+
+console.log(playa)
+console.log(playa.nominho)
+console.log(playa.idage)
+console.log(playa.racho)
+playa.Gadou(1)
+console.log(playa.racho)
